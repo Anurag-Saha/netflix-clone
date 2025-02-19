af70ab609ced12e1329b5ea06074610d2e8d0b62
Anurag-Saha <anuragsaha9183@gmail.com>
2025-02-19 16:43:15 +0530

    installation done

---
+++ src/home/Home.jsx
@@ -0,0 +1,12 @@
+// import { AcUnit } from "@mui/icons-material";
+import "./home.scss";
+
+const Home = () => {
+  return (
+    <div className="home">
+      hello
+          </div>
+  );
+};
+
+export default Home;
