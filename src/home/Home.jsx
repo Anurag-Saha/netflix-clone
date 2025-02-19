95e92462c7b13b73fa883af5d1c33951ac621901
Anurag-Saha <anuragsaha9183@gmail.com>
2025-02-19 17:26:12 +0530

    nav bar done

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -1,11 +1,16 @@
-// import { AcUnit } from "@mui/icons-material";
+import Navbar from "../components/navbar/Navbar";
 import "./home.scss";
 
 const Home = () => {
   return (
     <div className="home">
-      hello
-          </div>
+      <Navbar />
+      <img
+      width="100%"
+        src="https://images.pexels.com/photos/6899260/pexels-photo-6899260.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+        alt=""
+      />
+    </div>
   );
 };
 
